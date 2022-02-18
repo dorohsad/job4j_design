e2f0a509e14f94e7459086bbc6c1a3576784857a
SergeyDorokhov <dorohsad@mail.ru>
2022-02-18 22:13:58 +0300

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -0,0 +1,77 @@
+package ru.job4j.list;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements List<T> {
+
+    private T[] container;
+
+    private int size;
+
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length - 1) {
+            container = Arrays.copyOf(container, container.length * 2);
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        container[Objects.checkIndex(index, size)] = newValue;
+        return container[index];
+    }
+
+    @Override
+    public T remove(int index) {
+        T temp = container[Objects.checkIndex(index, size)];
+        System.arraycopy(container, index + 1,
+                container, index, container.length - index - 1);
+        container[container.length - 1] = null;
+        size--;
+        modCount++;
+        return temp;
+    }
+
+    @Override
+    public T get(int index) {
+        return container[Objects.checkIndex(index, size)];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            private int point;
+            private final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return point < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return container[point++];
+            }
+        };
+    }
+}
